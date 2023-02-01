7d289963b112806778595d0bab3270419720ac8d
Kyushik <kyushik95@gmail.com>
2023-02-01 13:37:18 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// function superbowlWin(array){
+//     for (const superbowl of array){
+//         if (superbowl.result === "W"){
+//             return superbowl.year
+//         }
+//     }
+// }
+
+function superbowlWin(array){
+    const object = array.find(element => element.result === "W")
+    if (object === undefined){
+        return undefined
+    }
+    return object.year
+}
